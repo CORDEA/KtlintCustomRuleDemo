8c444548b22834b32c3cea0bc5cfbcad687a344e
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-26 17:40:06 +0900

    add new rule

--- ktlint-rules/src/main/java/jp/cordea/ktlint/rules/CustomRuleSetProvider.kt
+++ ktlint-rules/src/main/java/jp/cordea/ktlint/rules/CustomRuleSetProvider.kt
@@ -4,5 +4,5 @@ import com.github.shyiko.ktlint.core.RuleSet
 import com.github.shyiko.ktlint.core.RuleSetProvider
 
 class CustomRuleSetProvider : RuleSetProvider {
-    override fun get(): RuleSet = RuleSet("empty", EmptyRule())
+    override fun get(): RuleSet = RuleSet("empty", EmptyRule(), ArgumentListParenthesisRule())
 }
